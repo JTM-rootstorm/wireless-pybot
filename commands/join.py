798caecfab339f441c3c1ded95fa2efddc0d2a86
tehguy <mgjustman@outlook.com>
2022-08-06 11:40:52 -0500

    fix bot fucky-wucky where it doesn't want to work after moving channels. Also have to keep list of VoiceClients because VoiceProtocols are useless and that's the only thing I'm allowed to pull from the context

--- commands/join.py
+++ commands/join.py
@@ -1,28 +1,30 @@
 import discord
+from discord import VoiceClient
 from discord.ext import commands
+from typing import Dict
 
 
 class Join(commands.Cog):
     def __init__(self, bot_: discord.Bot):
         self.bot = bot_
-
-    @commands.slash_command(name="join", description="fuck")
-    async def join(self, ctx: discord.ApplicationContext):
-        voice = ctx.author.voice
-
-        if voice is not None:
-            await voice.channel.connect()
-            await ctx.respond(f"Connected and bound to {voice.channel.mention}!")
-        else:
-            await ctx.respond(
-                "you need to be in a voice channel to use this"
-            )
+        self.isPlaying = False
+        self.voice_client_dict: Dict[int, VoiceClient] = {}
 
     @commands.slash_command(name="leave", description="leave the chat")
     async def leave(self, ctx: discord.ApplicationContext):
-        voice = ctx.voice_client
+        voice = self.voice_client_dict[ctx.author.guild.id]
+
         if voice is not None:
-            await voice.disconnect(force=False)
+            if not ctx.author.voice.channel:
+                await ctx.respond("You need to be in a VC to use this")
+                return
+
+            if ctx.author.voice.channel is not ctx.voice_client.channel:
+                await ctx.respond("We're not even in the same channel")
+                return
+
+            del self.voice_client_dict[ctx.author.guild.id]
+            await voice.disconnect(force=True)
             await ctx.respond(f"Left the VC")
         else:
             await ctx.respond("I'm not even in a channel")
@@ -30,9 +32,20 @@ class Join(commands.Cog):
     @commands.slash_command(name="play", description="play music")
     async def play(self, ctx: discord.ApplicationContext):
         voice_channel = ctx.author.voice.channel
+        guild_id = ctx.author.guild.id
+        voice_client = self.voice_client_dict.get(guild_id)
+
         if voice_channel is not None:
-            vc = await voice_channel.connect()
-            vc.play(
+            if voice_client is None:
+                self.voice_client_dict[guild_id] = await voice_channel.connect()
+                voice_client = self.voice_client_dict.get(guild_id)
+            else:
+                if voice_client.channel is not voice_channel:
+                    if voice_client.is_playing():
+                        voice_client.stop()
+                    await voice_client.move_to(voice_channel)
+
+            voice_client.play(
                 discord.FFmpegPCMAudio(
                     executable="ffmpeg.exe",
                     source=r"./media/test.mp3"
