50cbc34046ec57f8838c9949fd555df076e1aebf
tehguy <mgjustman@outlook.com>
2022-08-11 14:42:06 -0500

    closes #4

--- music.py
+++ music.py
@@ -30,8 +30,11 @@ class Music(commands.Cog):
         else:
             await ctx.respond("I'm not even in a channel")
 
-    @commands.slash_command(name="play2", description="play music")
-    async def play_static(self, ctx: discord.ApplicationContext):
+    @commands.slash_command(name="play", description="play from a URL")
+    async def play(self, ctx: discord.ApplicationContext, url: str):
+        await ctx.respond(f"Grabbing video...")
+        (video_title, video_duration, file_path) = youtubedl.get_video(url)
+
         voice_channel = ctx.author.voice.channel
         guild_id = ctx.author.guild.id
         voice_client = self.voice_client_dict.get(guild_id)
@@ -40,26 +43,20 @@ class Music(commands.Cog):
             if voice_client is None:
                 self.voice_client_dict[guild_id] = voice_client = await voice_channel.connect()
             else:
+                if voice_client.is_playing():
+                    voice_client.stop()
                 if voice_client.channel is not voice_channel:
-                    if voice_client.is_playing():
-                        voice_client.stop()
                     await voice_client.move_to(voice_channel)
 
             voice_client.play(
                 discord.FFmpegPCMAudio(
                     executable="ffmpeg.exe",
-                    source=r"./media/test.mp3"
+                    source=file_path
                 )
             )
-            await ctx.respond(f"Connected to {voice_channel.name}, playing audio")
+            await ctx.respond(f"Connected to {voice_channel.name}, playing {video_title}")
         else:
             await ctx.respond("you need to be in a voice channel to do this")
-
-    @commands.slash_command(name="play", description="play from a URL")
-    async def play(self, ctx: discord.ApplicationContext, url: str):
-        await ctx.respond(f"Grabbing video...")
-        (video_title, video_duration, file_path) = youtubedl.get_video(url)
-        await ctx.respond(f"Got \"{video_title}\", with duration of {video_duration}. It has been saved to {file_path}")
 
 
 def setup(bot):
